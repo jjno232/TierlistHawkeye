5c1fba313791701202e9abd3cf970dc188bb3ff8
june <132906902+jjno232@users.noreply.github.com>
2023-08-20 20:55:32 +0300

    Attempt release tag

--- .github/workflows/release_body.py
+++ .github/workflows/release_body.py
@@ -1,14 +1,13 @@
+import os
 import hashlib
 
-# Calculate MD5 hash of hawkeye.exe
-def calculate_md5(filename):
+def md5(filename):
     hasher = hashlib.md5()
     with open(filename, "rb") as file:
         for chunk in iter(lambda: file.read(4096), b""):
             hasher.update(chunk)
     return hasher.hexdigest()
 
-# Read Python script and extract version
 def extract_version(script_filename):
     version = None
     with open(script_filename, "r") as script_file:
@@ -20,8 +19,9 @@ def extract_version(script_filename):
 
 exe_filename = "hawkeye.exe"
 script_filename = "__main__.py"
-md5_hash = calculate_md5(exe_filename)
+md5_hash = md5(exe_filename)
 version = extract_version(script_filename)
+os.environ["RELEASE_VERSION"] = version
 body = f"""This release was built by GitHub Actions using untouched source code.
 **Version:** {version}
 **MD5:** {md5_hash}"""
